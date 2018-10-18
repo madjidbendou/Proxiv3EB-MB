b435281076d2a5ff02d73de99a6b9d78d1813af6
Maroua <maroua_ol@hotmail.com>
2018-10-18 14:59:38 +0200

    ignorejkhse

---
+++ target/classes/test-data.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO `Client` (`numero`, `rue`, `ville`, `login`, `motDePasse`, `id`) VALUES
+(36, 'avenue Cyrnos', 'Paris', 'log2', 'mdp2', 1),
+(36, 'avenue Cyrnos', 'Paris', 'log2', 'mdp2', 2);
+
+
+INSERT INTO `Personne` (`id`, `nom`, `prenom`) VALUES
+(1, 'toto', 'tata'),
+(2, 'Bernard', 'Tapie');
+
